49333019b3aaabe65a74982ba2f8d71240ddb093
AndJava2020 <stas.zerling@mail.ru>
2020-09-07 12:12:17 +0300

    Ex2

--- src/ru/AndJava2020/Ex2/DogHouse.java
+++ src/ru/AndJava2020/Ex2/DogHouse.java
@@ -3,12 +3,16 @@ package ru.AndJava2020.Ex2;
 import java.util.ArrayList;
 
 public class DogHouse {
-    private ArrayList<Dog> dogs=new ArrayList<Dog>();
+    private Dog[] dogs;
+    private int i=0;
     public DogHouse(){
-        dogs=new ArrayList<Dog>();
+        dogs=new Dog[10];
     }
-    public void add(Dog dog){
-        dogs.add(dog);
-        System.out.println(dog);
+    public void add(Dog ... dog){
+        int j=0;
+        for(;j< dog.length;i++,j++){
+            System.out.println(dog[j]);
+            dogs[i]=dog[j];
+        }
     }
 }
