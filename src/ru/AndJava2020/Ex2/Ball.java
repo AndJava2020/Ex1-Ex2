723a4454e0fcb44916777c0153616a8d4627ee75
AndJava2020 <stas.zerling@mail.ru>
2020-09-07 11:28:19 +0300

    Ex2

---
+++ src/ru/AndJava2020/Ex2/Ball.java
@@ -0,0 +1,10 @@
+package ru.AndJava2020.Ex2;
+
+public class Ball {
+    private int radius;
+    private String color;
+    public Ball(String color, int radius){
+        this.color=color;
+        this.radius=radius;
+    }
+}
---
+++ src/ru/AndJava2020/Ex2/Book.java
@@ -0,0 +1,10 @@
+package ru.AndJava2020.Ex2;
+
+public class Book {
+    private String name;
+    public int pages;
+    public Book(String name, int pages){
+        this.name=name;
+        this.pages=pages;
+    }
+}
